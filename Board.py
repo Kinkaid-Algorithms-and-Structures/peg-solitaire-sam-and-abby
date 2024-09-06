dd4ef7f2d5621a79bfcad4e49736c2a74a8561d6
samferber <samferber1@gmail.com>
2024-09-06 09:50:03 -0500

    Started move()

--- Board.py
+++ Board.py
@@ -10,25 +10,30 @@ class Board:
         self.board[2]=[blank,blank,(4,True),blank,(5,True),blank,(6,True),blank,blank]
         self.board[3]=[blank,(7,True),blank,(8,True),blank,(9,True),blank,(10,True),blank]
         self.board[4]=[(11,True),blank,(12,True),blank,(13,True),blank,(14,True),blank,(15,True)]
-    def move(self,id1,id2):
-        pass
+    def move(self, x1,y1, direction):
+        point1 = self.board[y1][x1]
+        point2= self.check_if_legal(x1,y1, direction)[2]
+        midpoint = self.check_if_legal(x1, y1, direction)[1]
+
+
 
     def check_if_legal(self,x1,y1,direction):
         point1=self.board[y1][x1]
+        blank = (0,0)
         if point1[1]:
             if direction=="right":
                 if x1>=5 :
-                    return False
+                    return False, blank, blank
                 point2=self.board[y1][x1+4]
                 midpoint=self.board[y1][x1+2]
             elif direction=="left":
                 if x1<=3:
-                    return False
+                    return False, blank, blank
                 point2 = self.board[y1][x1 - 4]
                 midpoint = self.board[y1][x1 - 2]
             elif direction=="top right":
                 if x1>=7 and y1<=1 :
-                    return False
+                    return False, blank, blank
                 point2 = self.board[y1-2][x1 +2]
                 midpoint = self.board[y1-1][x1 +1]
             elif direction=="top left":
@@ -37,24 +42,24 @@ class Board:
                     midpoint = self.board[y1-1][x1 - 1]
             elif direction=="bottom right":
                 if x1>=7 and y1<=3 :
-                    return False
+                    return False, blank, blank
                 point2 = self.board[y1+2][x1 +2]
                 midpoint = self.board[y1+1][x1 +1]
             elif direction=="bottom left":
                 if x1 <= 1 and y1 <= 3:
-                    return False
+                    return False, blank, blank
                 point2 = self.board[y1 + 2][x1 - 2]
                 midpoint = self.board[y1 + 1][x1 - 1]
             else:
-                return False
+                return False, blank, blank
             if point2[0] == 0:
-                return False
+                return False, blank, blank
             elif point2[1]:
-                return False
+                return False, blank, blank
             elif not midpoint[1]:
-                return False
+                return False, blank, blank
             else:
-                return True
+                return True, midpoint, point2
 
 
 
