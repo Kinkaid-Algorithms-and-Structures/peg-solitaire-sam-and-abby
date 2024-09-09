1a496ea7dfbcc932dfe8ec1df40f43d6c8e6079e
samferber <samferber1@gmail.com>
2024-09-09 10:02:56 -0500

    Fixed board with lists instead of tuples

--- Board.py
+++ Board.py
@@ -4,12 +4,12 @@ class Board:
         for i in range(0,4):
             new_row=[]
             self.board.append(new_row)
-        blank=(0,False)
-        self.board[0]=[blank,blank,blank,blank,(1,False),blank,blank,blank,blank]
-        self.board[1]=[blank,blank,blank,(2,True),blank,(3,True),blank,blank,blank]
-        self.board[2]=[blank,blank,(4,True),blank,(5,True),blank,(6,True),blank,blank]
-        self.board[3]=[blank,(7,True),blank,(8,True),blank,(9,True),blank,(10,True),blank]
-        self.board[4]=[(11,True),blank,(12,True),blank,(13,True),blank,(14,True),blank,(15,True)]
+        blank=[0,False]
+        self.board[0]=[blank,blank,blank,blank,[1,False],blank,blank,blank,blank]
+        self.board[1]=[blank,blank,blank,[2,True],blank,[3,True],blank,blank,blank]
+        self.board[2]=[blank,blank,[4,True],blank,[5,True],blank,[6,True],blank,blank]
+        self.board[3]=[blank,[7,True],blank,[8,True],blank,[9,True],blank,[10,True],blank]
+        self.board[4]=[[11,True],blank,[12,True],blank,[13,True],blank,[14,True],blank,[15,True]]
     def move(self, x1,y1, direction):
         #assumes the move has already been checked by check_if_legal
         point1 = self.board[y1][x1]
