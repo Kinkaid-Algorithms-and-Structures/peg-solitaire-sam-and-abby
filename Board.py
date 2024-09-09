a4cbc1fa8b1528250e889195b1b9d11a2d8f40e5
samferber <samferber1@gmail.com>
2024-09-09 10:10:16 -0500

    started Draw

--- Board.py
+++ Board.py
@@ -64,6 +64,10 @@ class Board:
                 return False, blank, blank
             else:
                 return True, midpoint, point2
+    def draw(self):
+        for row in self.board:
+            for column in row:
+                pass
 
 
 
--- PegSolitaireRunner.py
+++ PegSolitaireRunner.py
@@ -1,45 +1,10 @@
 import logging
 import datetime
 from KinkaidDecorators import log_start_stop_method
+from Board import Board
 
 logging.basicConfig(level=logging.INFO)
 
-class Board:
-    def __init__(self):
-        self.board = [['_'] * 5 for _ in range(5)]  # example board setup (adjust size and initialization as necessary)
-        self.setup_board()
-
-    def setup_board(self):
-        # initialize the board with pegs (except one empty hole for starting the game)
-        for i in range(5):
-            for j in range(5):
-                self.board[i][j] = 'o'
-        self.board[2][2] = '_'  # empty middle spot for example
-
-    def draw(self):
-        for row in self.board:
-            print(' '.join(row))
-
-    def ask_for_move(self):
-        return input("Enter your move (format 'x1 y1 direction'): ")  ## change to include direction
-
-    def make_move(self, x1, y1, direction):
-        # is this right?
-        directions = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
-        dx, dy = directions[direction]
-        x2, y2 = x1 + 2 * dx, y1 + 2 * dy
-        self.board[x2][y2] = 'o'
-        self.board[x1][y1] = '_'
-        self.board[x1 + dx][y1 + dy] = '_'  # remove the jumped peg
-
-    def is_move_legal(self, x1, y1, direction):
-        # add the logic to check if a move is legal
-        directions = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
-        dx, dy = directions[direction]
-        x2, y2 = x1 + 2 * dx, y1 + 2 * dy
-        if 0 <= x2 < 5 and 0 <= y2 < 5 and self.board[x1 + dx][y1 + dy] == 'o' and self.board[x2][y2] == '_':
-            return True
-        return False
 
 class PegSolitaireRunner:
     def __init__(self):
@@ -51,10 +16,10 @@ class PegSolitaireRunner:
         ## move and check if legal input is the x and the y coordinate of the peg youre trying to move and the direction as a string you are trying to move
         while True:  # simple loop --> needs more conditions based on actual game rules
             self.board.draw()
-            move = self.board.ask_for_move().split()
+            move = self.board.move().split()
             x1, y1, direction = int(move[0]), int(move[1]), move[2]
-            if self.board.is_move_legal(x1, y1, direction):
-                self.board.make_move(x1, y1, direction)
+            if self.board.check_if_legal(x1, y1, direction):
+                self.board.move(x1, y1, direction)
                 logging.info(f"Move from ({x1}, {y1}) in direction '{direction}' executed.")
             else:
                 logging.info("Illegal move attempted.")
